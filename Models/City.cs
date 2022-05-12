e72091b8f1d82dd64c7669b104a3b3428da06f91
kamra <kamra@LAPTOP-M49P7DNF>
2022-05-12 17:17:51 +0400

    first commit

---
+++ Models/City.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Country_City_Population.Models
+{
+    class City
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Country_City_Population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
